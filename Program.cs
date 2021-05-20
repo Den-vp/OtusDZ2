993a629488afdf80293d9dba33d13c68912ba5d5
Denis Vlasov <Den_vp@mail.ru>
2021-05-20 15:44:24 +0600

    commit 3

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ namespace Otus.CSharp.HW2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Приложение для личного финансового учета");
+
             Trace.Listeners.Add(new ConsoleTraceListener());
 
             var currencyConverter = new ExchangeRatesApiConverter(new HttpClient(), new MemoryCache(new MemoryCacheOptions()), "a5cf9da55cb835d0a633a7825b3aa8b5");
@@ -21,23 +23,21 @@ namespace Otus.CSharp.HW2
 
             var budgetApp = new BudjetApplication(transactionRepository, transactionParser, currencyConverter);
 
-            //budgetApp.AddTransaction("Трата -40 EUR Продукты Пятерочка");
-            //budgetApp.AddTransaction("Трата -200 EUR Бензин IRBIS");
-            //budgetApp.AddTransaction("Трата -50 EUR Кафе Шоколадница");
-            //budgetApp.AddTransaction("Зачисление 5000 EUR Зарплата");
-            //budgetApp.AddTransaction("Перевод 12.3 EUR Вася Долг");
+            //budgetApp.AddTransaction("Трата -10 EUR Продукты A-Store");
+            //budgetApp.AddTransaction("Трата -100 USD Бензин RP");
+            //budgetApp.AddTransaction("Трата -50 EUR Кафе Turandot");
+            //budgetApp.AddTransaction("Зачисление 2000 EUR Зарплата");
+            //budgetApp.AddTransaction("Перевод 50 EUR Услуги BeeLine");
 
             budgetApp.OutputTransactions();
 
             budgetApp.OutputBalanceInMainCurrencies();
 
             var flagContinue = true;
-
-            Console.WriteLine("");
             
             while (flagContinue)
             {
-                Console.WriteLine("Press 1 - to input new transaction, 2 - print transactions, 0 - to exit");
+                Console.WriteLine("Введите 1 - для ввода новой транзакции, 2 - вывести транзакции, 0 - Выход");
                 var key = Console.ReadKey();
                 Console.WriteLine();
 
@@ -47,7 +47,7 @@ namespace Otus.CSharp.HW2
                 }
                 else if (key.KeyChar == '1')
                 {
-                    Console.WriteLine("Input new transaction:");
+                    Console.WriteLine("Введите транзакцию: ");
                     var input = Console.ReadLine();
 
                     budgetApp.AddTransaction(input);
@@ -56,7 +56,7 @@ namespace Otus.CSharp.HW2
                 }
                 else if (key.KeyChar == '2')
                 {
-                    Console.WriteLine("Saved transactions:");
+                    Console.WriteLine("Список транзакций:");
                     Console.WriteLine("======================");
 
                     budgetApp.OutputTransactions();
